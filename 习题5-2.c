b669ca9a802c22c79f756a309dab4ef12093b814
acetao <acetao@163.com>
2022-08-03 15:36:32 +0800

    习题5-2 使用函数求奇数和

---
+++ 习题5-2.c
@@ -0,0 +1,13 @@
+int even( int n )
+{
+    return n % 2 == 0 ? 1 : 0;
+}
+int OddSum( int List[], int N )
+{
+    int sum = 0;
+    for (int i = 0; i < N; i++) {
+        if (even(List[i]) == 0)
+            sum += List[i];
+    }
+    return sum;
+}
