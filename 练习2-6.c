a3e12d7c3f6e0738526b3c8844cc650cf0f2a243
acetao <acetao@163.com>
2023-08-19 22:47:57 +0800

    练习2-6 计算物体自由下落的距离

---
+++ 练习2-6.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    double g = 10, t = 3;
+    double h = g * t * t / 2;
+    printf("height = %.2lf\n", h);
+
+  return 0;
+}
